969ee16fef1fa478ed8b4d38bb19e3dd7509864a
Marco Antonio <marcospatton@hotmail.com>
2021-09-20 13:04:19 -0300

    adicionando arquivo js

---
+++ js/scripts.js
@@ -0,0 +1,88 @@
+
+/*function mostra(id){
+    if(document.getElementById(id).style.display =='block'){
+        document.getElementById(id).style.display = 'none';
+        document.getElementById('b'+id).value="Leia Mais";
+
+    }else {document.getElementById(id).style.display ='block';
+         document.getElementById('b'+id).value="Leia Menos";}
+}*/
+
+/*function leiaMais(){
+    
+    var pontos=document.getElementById("pontos");
+    var maisTexto=document.getElementById("mais");
+    var btnLeiaMais=document.getElementById("btnLeiaMais");
+
+
+    if(pontos.style.display === "none"){
+
+        pontos.style.display="inline";
+
+        maisTexto.style.display="none";
+
+        btnLeiaMais.innerHTML="Leia Mais";
+
+
+    }else{
+
+        pontos.style.display="none";
+
+        maisTexto.style.display ="inline";
+
+        btnLeiaMais.innerHTML="Leia Menos";
+    }
+}
+
+function leiaMais_dois(){
+    
+    var pontos_dois=document.getElementById("pontos_dois");
+    var maisTexto_dois=document.getElementById("mais_dois");
+    var btnLeiaMais_dois=document.getElementById("btnLeiaMais_dois");
+
+
+    if(pontos_dois.style.display === "none"){
+
+        pontos_dois.style.display="inline";
+
+        maisTexto_dois.style.display="none";
+
+        btnLeiaMais_dois.innerHTML="Leia Mais";
+
+
+    }else{
+
+        pontos_dois.style.display="none";
+
+        maisTexto_dois.style.display ="inline";
+
+        btnLeiaMais_dois.innerHTML="Leia Menos";
+    }
+}
+
+function leiaMais_tres(){
+    
+    var pontos_tres=document.getElementById("pontos_tres");
+    var maisTexto_tres=document.getElementById("mais_tres");
+    var btnLeiaMais_tres=document.getElementById("btnLeiaMais_tres");
+
+
+    if(pontos_tres.style.display === "none"){
+
+        pontos_tres.style.display="inline";
+
+        maisTexto_tres.style.display="none";
+
+        btnLeiaMais_tres.innerHTML="Leia Mais";
+
+
+    }else{
+
+        pontos_tres.style.display="none";
+
+        maisTexto_tres.style.display ="inline";
+
+        btnLeiaMais_tres.innerHTML="Leia Menos";
+    }
+}
+*/
